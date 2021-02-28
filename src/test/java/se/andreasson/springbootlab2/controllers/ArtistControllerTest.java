7e702ecb69bb82ed4c0edddc94c0fc5ff148980e
Petra Andreasson <petra.andreasson@iths.se>
2021-02-28 16:24:22 +0100

    Added use of TestConfiguration in ArtistControllerTest class whan calling ArtistController. Update pom. Draft docker-compose

--- src/test/java/se/andreasson/springbootlab2/controllers/ArtistControllerTest.java
+++ src/test/java/se/andreasson/springbootlab2/controllers/ArtistControllerTest.java
@@ -3,6 +3,7 @@ package se.andreasson.springbootlab2.controllers;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions.*;
 import org.springframework.web.server.ResponseStatusException;
+import se.andreasson.springbootlab2.configurations.TestConfiguration;
 import se.andreasson.springbootlab2.services.TestService;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -12,7 +13,7 @@ class ArtistControllerTest {
 
     @Test
     void callingOneWithValidIdReturnOneArtist() {
-        ArtistController artistController = new ArtistController(new TestService());
+        ArtistController artistController = new ArtistController(new TestConfiguration(), new TestService());
         var artist = artistController.one(2L);
 
         assertThat(artist.getArtistId()).isEqualTo(2);
@@ -20,7 +21,7 @@ class ArtistControllerTest {
     }
 
     void callingOneWithInvalidIdThrowsNotFoundException() {
-        ArtistController artistController = new ArtistController((new TestService()));
+        ArtistController artistController = new ArtistController(new TestConfiguration(), new TestService());
 
         var artist = artistController.one(1L);
         assertThrows(ResponseStatusException.class, () -> artistController.one(1L));
